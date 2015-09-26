fea782a8814671f5395165019b2e3399872fb440
grecinto <gerardorecinto@yahoo.com>
2015-09-26 10:01:19 -0700

    Added ResultEvaluator demo.

--- SampleTracerApp/Program.cs
+++ SampleTracerApp/Program.cs
@@ -15,8 +15,16 @@ namespace SampleTracerApp
 
             Console.WriteLine("Running Enterprise Logging Tracer demo.");
             DemoEnterpriseTracer();
-            Console.WriteLine("Pls. check your MSEL log file for messages logged by the demo.");
             Console.WriteLine();
+            Console.WriteLine("Running Enterprise Logging Trace Scope Using ResultsEval demo.");
+            DemoEnterpriseTracerWithScopeAndResultEval();
+            Console.WriteLine("Pls. check your MSEL log file for messages logged by Enterprise Logging demo.");
+            Console.WriteLine();
+
+            Console.WriteLine("Running Tracer with Scope and ResultsEval to Console demo.");
+            DemoTracerWithScopeAndResultEval();
+            Console.WriteLine();
+
             Console.WriteLine("Running Tracer Logging to Console demo.");
             DemoTracerToConsole();
             Console.WriteLine();
@@ -33,6 +41,7 @@ namespace SampleTracerApp
             Console.ReadLine();
         }
 
+        #region demo code
         private static void DemoEnterpriseTracer()
         {
             ResultEvaluatorDelegate re = (object result, TimeSpan? runTime, out string customMessage) =>
@@ -41,9 +50,29 @@ namespace SampleTracerApp
                 return ResultActionType.Default;
             };
 
-            EnterpriseTracer Tracer = new EnterpriseTracer(category: new string[] { "DemoTraceScopeToConsole" },
-                resultEvaluator: re);
+            EnterpriseTracer Tracer = new EnterpriseTracer(new string[] { "DemoEnterpriseTracer" }, re);
             Tracer.Invoke(Foo, funcFootprint: "Foo()");
+        }
+
+        private static void DemoEnterpriseTracerWithScopeAndResultEval()
+        {
+            // Shows how to dictate generated log message using a ResultsEvaluator.
+            EnterpriseTracer Tracer = new EnterpriseTracer(new string[] { "DemoEnterpriseTracerWithScopeAndResultEval" }, resultEval);
+            using (new Tracing.MSEnterpriseLogging.TraceScope(Tracer, "FooTakesTime(5)"))
+            {
+                FooTakesTime(5);
+            }
+        }
+        private static void DemoTracerWithScopeAndResultEval()
+        {
+            // Shows how to dictate generated log message using a ResultsEvaluator.
+            var Tracer = new Tracing.Tracer(new string[] { "DemoTracerWithScopeAndResultEval" }, resultEval);
+            Tracer.OnLog += Tracer_OnLog;
+            Tracer.OnException += Tracer_OnException;
+            using (new Tracing.TraceScope(Tracer, "FooTakesTime(4)"))
+            {
+                FooTakesTime(4);
+            }
         }
 
         private static void DemoTraceScopeToConsole()
@@ -52,7 +81,7 @@ namespace SampleTracerApp
             var Tracer = new Tracing.Tracer(category: new string[] { "DemoTraceScopeToConsole" });
             Tracer.OnLog += Tracer_OnLog;
             Tracer.OnException += Tracer_OnException;
-            using (new TraceScope<Tracing.Tracer>(Tracer, "Foo()"))
+            using (new Tracing.TraceScope(Tracer, "Foo()"))
             {
                 // invoke Foo via Tracer to generate code tracing useful logs on Enter, [on Exit].
                 // run time measurement is also logged on generated "on Exit" logs.
@@ -62,11 +91,12 @@ namespace SampleTracerApp
             var i = 123;
             try
             {
-                using (new TraceScope<Tracing.Tracer>(Tracer, string.Format("FooThatThrows({0})", i),
+                using (new Tracing.TraceScope(Tracer, string.Format("FooThatThrows({0})", i),
                     InvokeVerbosity.OnEnter))
                 {
                     FooThatThrows(i);
-                    // NOTE: OnLeave will not be invoked because verbosity is set to "OnEnter" only.
+                    // NOTE: OnLeave will not be invoked (i.e. - no OnLeave log generated)
+                    // because verbosity is set to "OnEnter" only.
                 }
             }
             catch
@@ -79,7 +109,7 @@ namespace SampleTracerApp
         private static void DemoTraceScopeUsingSingletonTracer()
         {
             // hook up console writer/logger for this demo!
-            var Tracer = new Tracing.Tracer(category: new string[] { "DemoTraceScopeAndSingletonTracer" });
+            var Tracer = new Tracing.Tracer(category: new string[] { "DemoTraceScopeUsingSingletonTracer" });
             Tracer.OnLog += Tracer_OnLog;
             Tracer.OnException += Tracer_OnException;
             // Call SetTracer to setup singleton Tracer Instance.
@@ -124,11 +154,36 @@ namespace SampleTracerApp
             Tracer.Invoke(FooThatThrows, i, InvokeVerbosity.OnEnter,
                 funcFootprint: string.Format("FooThatThrows({0})", i));
         }
+        #endregion
+
+        /// <summary>
+        /// Result Evaluator sample method.
+        /// </summary>
+        /// <param name="result"></param>
+        /// <param name="runTime"></param>
+        /// <param name="customMessage"></param>
+        /// <returns></returns>
+        private static ResultActionType resultEval(object result, TimeSpan? runTime, out string customMessage)
+        {
+            if (runTime.Value.TotalSeconds >= 5)
+            {
+                // log a failed warning msg if method took more than 4 seconds.
+                customMessage = "Warning! Method took more than 4 secs to run.";
+                return ResultActionType.Fail;
+            }
+            // log a passed msg if method ran faster than 5 seconds.
+            customMessage = null;
+            return ResultActionType.Pass;
+        }
+
 
         private static void Tracer_OnException(Exception exc, string functionInfo)
         {
             var msg = string.Format("{0}, details: {1}", functionInfo, exc.ToString());
             Tracer_OnLog(LogLevels.Fatal, null, msg);
+
+            // you can rethrow exc if needed.
+            //throw exc;
         }
         private static void Tracer_OnLog(LogLevels logLevel, string[] category, string message)
         {
@@ -136,7 +191,7 @@ namespace SampleTracerApp
             string cat = "None";
             if (category != null)
                 cat = string.Join(", ", category);
-            var msg = string.Format("{0}: {1}, Category: {2}, Message: {3}", DateTime.Now, logLevel, cat, message);
+            var msg = string.Format("{0}: {1}: {2}: {3}", DateTime.Now, cat, logLevel, message);
             Console.WriteLine(msg);
         }
 
@@ -144,6 +199,13 @@ namespace SampleTracerApp
         {
             int a = 1 + 2;
             Tracer_OnLog(LogLevels.Information, null, "Inside Foo.");
+            return a;
+        }
+        static int FooTakesTime(int delayTimeInSec = 5)
+        {
+            int a = 1 + 2;
+            Tracer_OnLog(LogLevels.Information, null, string.Format("Sleeping for {0} secs in FooTakesTime.", delayTimeInSec));
+            System.Threading.Thread.Sleep(delayTimeInSec * 1000);
             return a;
         }
         static int FooThatThrows(int arg1)
